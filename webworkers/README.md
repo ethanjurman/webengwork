a667352bd3957cc61feb37106186d2e1a1e89ca7
Ethan H. Jurman <ehj2229@rit.edu>
2016-04-18 13:10:28 -0400

    some comments and documentation to run web workers

---
+++ webworkers/README.md
@@ -0,0 +1,4 @@
+web workers easy!
+`npm install`
+`npm start`
+go to `http://localhost:3000/webworkers.html`
--- webworkers/server.js
+++ webworkers/server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var server = express();
-server.use(express.static('static'))
+server.use(express.static('static'));
+// treats you're "/static" folder as a server running on localhost:3000,
+// which means that if you put it in any server (like on the SE domain), it should
+// run just fine!
 server.listen(3000);
